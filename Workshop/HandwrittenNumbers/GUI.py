aa711f514160b41af32da87e6abdb7f8d20a954d
Omkar Khilari <omkhilariindia@gmail.com>
2020-11-04 09:13:04 +0530

    Started Face detection

--- Workshop/HandwrittenNumbers/GUI.py
+++ Workshop/HandwrittenNumbers/GUI.py
@@ -4,11 +4,12 @@ import numpy as np
 import cv2
 import joblib
 count=0
+model=joblib.load("/Users/mac/Desktop/Simple DSP/Workshop/HandwrittenNumbers/KNN_model.sav")
 
 def eventFunction(event):
    x=event.x
    y=event.y
-   fat=15
+   fat=25
 
    x1=x-fat
    y1=y-fat
@@ -23,19 +24,36 @@ def save():
 
    imgArray=np.array(img)
    imgArray=cv2.resize(imgArray,(8,8))
-
-   cv2.imwrite("HandwrittenNumbers/data/"+str(count)+".jpg",imgArray)
+   print(imgArray)
+   if not cv2.imwrite("/Users/mac/Desktop/Simple DSP/Workshop/HandwrittenNumbers/data/"+str(count)+".jpg",imgArray):
+      raise Exception("Error Writing Image")
    count+=1
 
 def clear():
+   global img,imgDraw
+
    canvas.delete("all")
-   img=Image.new("RGB",(w,h),(255,255,255))
+   img=Image.new("RGB",(w,h),(0,0,0))
    imgDraw=ImageDraw.Draw(img)
+
+def predict():
+   global img
+   print(img)
+   imgArray=np.array(img)
+   # print(imgArray)
+   imgArray=cv2.cvtColor(imgArray,cv2.COLOR_RGB2GRAY)
+   imgArray=cv2.resize(imgArray,(8,8))
+   imgArray=np.reshape(imgArray,(1,64))
+   print(imgArray)
+   imgArray=(imgArray/255)*16
+   # print(imgArray)
+   result=model.predict(imgArray)
+   labelStatus.config(text="PREDICTED DIGIT"+str(result))
    
 
 #Main Window
 win=tk.Tk()
-w,h=200,300
+w,h=300,300
 f="Helvetica 20 bold"
 f_label="Helvetica 24 bold"
 
@@ -45,10 +63,10 @@ canvas.grid(row=0,column=0,columnspan=4)
 buttonSave=tk.Button(win,text="SAVE",bg="pink",fg="white",font=f,command=save)
 buttonSave.grid(row=1,column=0)
 
-buttonSave=tk.Button(win,text="PREDICT",bg="blue",fg="White",font=f)
+buttonSave=tk.Button(win,text="PREDICT",bg="blue",fg="White",font=f,command=predict)
 buttonSave.grid(row=1,column=1)
 
-buttonSave=tk.Button(win,text="CLEAR",bg="gold",fg="White",font=f)
+buttonSave=tk.Button(win,text="CLEAR",bg="gold",fg="White",font=f,command=clear)
 buttonSave.grid(row=1,column=2)
 
 buttonSave=tk.Button(win,text="EXIT",bg="red",fg="White",font=f)
@@ -58,7 +76,7 @@ labelStatus=tk.Label(win,text="PREDICTED DIGIT: NONE",bg="white",fg="black",font
 labelStatus.grid(row=2,column=0,columnspan=4)
 
 canvas.bind("<B1-Motion>",eventFunction)
-img=Image.new("RGB",(w,h),(255,255,255))
+img=Image.new("RGB",(w,h),(0,0,0))
 imgDraw=ImageDraw.Draw(img)
 
 win.mainloop()
