7b9df94c7785352cc3329d22448fcd67d5f0b29b
Omkar Khilari <omkhilariindia@gmail.com>
2021-01-26 12:36:32 +0530

    .

--- ML_From_Scratch/KNN/Test.py
+++ ML_From_Scratch/KNN/Test.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import numpy as np
 
+# Simple accuracy function 
 def accuracy(true, pred):
     accuracy = np.sum(true == pred) / len(true)
     return accuracy
@@ -10,9 +11,9 @@ def accuracy(true, pred):
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-k = 3
-clf = KNN(K=k)
-clf.fit(X_train, y_train)
-predictions = clf.predict(X_test)
-print("Accuracy", accuracy(y_test, predictions))
+# Splitting the data to train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+KNNmodel = KNN(K=5)
+KNNmodel.fit(X_train, y_train)
+predictions = KNNmodel.predict(X_test)
+print("Accuracy-", accuracy(y_test, predictions))
